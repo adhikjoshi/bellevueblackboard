369eff65e12e400ff0cc282e9ac27c7d7de35dab
tslater2006@gmail.com <tslater2006@gmail.com@1e1beb38-95ba-4bfd-f5cd-75410889f4e4>
2011-03-14 18:14:13 +0000

    Tied Logging in to the GUI, will now log users in and display if it was successful or not.

--- trunk/src/edu/bellevue/android/MainActivity.java
+++ trunk/src/edu/bellevue/android/MainActivity.java
@@ -1,7 +1,12 @@
 package edu.bellevue.android;
 
+import edu.bellevue.android.blackboard.BlackboardHelper;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,28 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        findViewById(R.id.btnLogIn).setOnClickListener(new submitListener());
+    }
+    
+    // Listener for the submit button
+    private class submitListener implements OnClickListener
+    {
+    	public void onClick(View v) {
+    		// get references to the Edit Boxes
+    		EditText userName = ((EditText)findViewById(R.id.txtUserName));
+    		EditText password = ((EditText)findViewById(R.id.txtPassword));
+    		
+    		// Attempt to log in
+    		BlackboardHelper.logIn(userName.getText().toString(), password.getText().toString());
+    		
+    		if (BlackboardHelper.isLoggedIn())
+    		{
+    			// Display if successful
+    			Toast.makeText(MainActivity.this, "Logged In!", Toast.LENGTH_LONG).show();
+    		}else
+    		{
+    			Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_LONG).show();
+    		}
+    	}
     }
 }
