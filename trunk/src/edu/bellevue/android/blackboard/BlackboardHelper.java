a19f3531fd09158001c464fad864ccc05bbb4b06
tslater2006@gmail.com <tslater2006@gmail.com@1e1beb38-95ba-4bfd-f5cd-75410889f4e4>
2011-03-14 15:08:07 +0000

    Added Log-In functionality (code only, not tied to UI yet)

---
+++ trunk/src/edu/bellevue/android/blackboard/BlackboardHelper.java
@@ -0,0 +1,150 @@
+package edu.bellevue.android.blackboard;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+
+public class BlackboardHelper {
+
+	private static final String LOGIN_URL = "https://cyberactive.bellevue.edu/webapps/login/";
+	private static HttpClient client = null;
+	private static boolean _loggedIn = false;
+	private static HttpResponse httpResponse = null;
+	private static HttpPost httpPost = null;
+	
+	public static boolean logIn(String userName, String password)
+	{
+		try{
+
+		httpPost = new HttpPost(LOGIN_URL);
+		List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+		if (client != null){
+			client.getConnectionManager().shutdown();
+			client = null;
+		}
+
+		client = createHttpClient();
+
+		// by parsing out the HTML page and looking at the code
+		// we know that the password is base64 encoded twice (once ansii once unicode)
+		// and that the password field itself is nulled out.
+        nvps.add(new BasicNameValuePair("action", "login"));
+        nvps.add(new BasicNameValuePair("user_id",userName));
+        nvps.add(new BasicNameValuePair("encoded_pw",Base64.encodeBytes(password.getBytes(), 0)));
+        
+        // dont really have a Unicode base64 utility but blackboard seems OK with this
+        nvps.add(new BasicNameValuePair("encoded_pw_unicode",Base64.encodeBytes(password.getBytes(), 0)));
+        nvps.add(new BasicNameValuePair("remote-user", ""));
+        nvps.add(new BasicNameValuePair("new_loc", ""));
+        nvps.add(new BasicNameValuePair("auth_type", ""));
+        nvps.add(new BasicNameValuePair("one_time_token", ""));
+        nvps.add(new BasicNameValuePair("pass", ""));
+
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+
+        // try to log in
+        httpResponse = client.execute(httpPost);
+        
+        // check for "Could not login" showing that it failed.
+        
+        if (convertStreamToString(httpResponse.getEntity().getContent()).contains("Could not login"))
+		{ 
+        	_loggedIn = false;
+		}else
+		{
+			_loggedIn = true;
+		}
+        
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			_loggedIn = false;
+		}
+		return _loggedIn;
+	}
+	public static boolean isLoggedIn()
+	{
+		return _loggedIn;
+	}
+
+// PRIVATE HELPER METHODS
+
+	private static HttpClient createHttpClient() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException
+    {
+		// This function will create the HttpClient we need to use for blackboard
+		// this uses our MySSLSocketFactory to prevent cert checking
+		
+        HttpParams params = new BasicHttpParams();
+        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+        HttpProtocolParams.setContentCharset(params, HTTP.DEFAULT_CONTENT_CHARSET);
+        HttpProtocolParams.setUseExpectContinue(params, true);
+
+        SchemeRegistry schReg = new SchemeRegistry();
+        schReg.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+        schReg.register(new Scheme("https", new MySSLSocketFactory(null), 443));
+        ClientConnectionManager conMgr = new ThreadSafeClientConnManager(params, schReg);
+        DefaultHttpClient d = new DefaultHttpClient(conMgr, params);
+        
+        // Blackboard refuses to allow mobile agents, so we spoof it to Firefox :P
+        d.getParams().setParameter("http.useragent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.107 Safari/534.13");
+        return d;
+    }
+	private static String convertStreamToString(InputStream is)
+    throws IOException {
+		// NOT MY CODE!
+		// This function effectively converts the HTTP response into a string
+		/*
+		 * To convert the InputStream to String we use the
+		 * Reader.read(char[] buffer) method. We iterate until the
+		 * Reader return -1 which means there's no more data to
+		 * read. We use the StringWriter class to produce the string.
+		 */
+		if (is != null) {
+		    Writer writer = new StringWriter();
+		
+		    char[] buffer = new char[1024];
+		    try {
+		        Reader reader = new BufferedReader(
+		                new InputStreamReader(is, "UTF-8"));
+		        int n;
+		        while ((n = reader.read(buffer)) != -1) {
+		            writer.write(buffer, 0, n);
+		        }
+		    } finally {
+		        is.close();
+		    }
+		    return writer.toString();
+		} else {        
+		    return "";
+		}
+    }
+	
+}
