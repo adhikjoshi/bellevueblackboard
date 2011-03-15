cf2be26fcdb33399d02e690d54e219548dd30a98
tslater2006@gmail.com <tslater2006@gmail.com@1e1beb38-95ba-4bfd-f5cd-75410889f4e4>
2011-03-15 19:07:45 +0000

    Added the Course Viewer, after logging in it will display all enrolled classes

---
+++ src/edu/bellevue/android/CourseActivity.java
@@ -0,0 +1,74 @@
+package edu.bellevue.android;
+
+import java.util.List;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.widget.ArrayAdapter;
+import edu.bellevue.android.blackboard.BlackboardHelper;
+import edu.bellevue.android.blackboard.Course;
+
+public class CourseActivity extends ListActivity {
+
+	private static final int THREAD_COMPLETE = 1;
+	private static final int CONN_NOT_ALLOWED = 2;
+	private List<Course> courses;
+	private SharedPreferences prefs;
+	private Context ctx;
+	private Handler handler;
+	private ProgressDialog pd;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.listview);
+	    ctx = this.getApplicationContext();
+	    prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+	    handler = new threadHandler();
+	    
+	    pd = ProgressDialog.show(this, "Please Wait", "Loading Courses...");
+	    
+	    Thread t = new Thread(new getCoursesThread());
+	    t.start();
+	    
+	}
+	
+	
+	private class threadHandler extends Handler
+	{
+		public void handleMessage(Message m)
+		{
+			switch(m.what)
+			{
+			case THREAD_COMPLETE:
+				pd.dismiss();
+				setListAdapter(new ArrayAdapter<Course>(ctx, android.R.layout.simple_list_item_1,courses));
+			case CONN_NOT_ALLOWED:
+			}
+		}
+	}
+	
+	private class getCoursesThread implements Runnable
+	{
+
+		public void run() {
+			if (ConnChecker.shouldConnect(prefs, ctx))
+			{
+				courses = BlackboardHelper.getCourses();
+				handler.sendEmptyMessage(THREAD_COMPLETE);
+			}else
+			{
+				ConnChecker.showUnableToConnect(CourseActivity.this);
+				handler.sendEmptyMessage(CONN_NOT_ALLOWED);
+			}
+			
+		}
+		
+	}
+}
