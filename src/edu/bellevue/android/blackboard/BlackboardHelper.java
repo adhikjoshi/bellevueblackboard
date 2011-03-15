0ec728296860d67f8ecde257ff6ac2bf0611a8a5
tslater2006@gmail.com <tslater2006@gmail.com@1e1beb38-95ba-4bfd-f5cd-75410889f4e4>
2011-03-15 19:31:11 +0000

    cleaned up imports (didnt need File and FileOutputStream)

--- src/edu/bellevue/android/blackboard/BlackboardHelper.java
+++ src/edu/bellevue/android/blackboard/BlackboardHelper.java
@@ -1,8 +1,6 @@
 package edu.bellevue.android.blackboard;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
